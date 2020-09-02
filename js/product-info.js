8ea4bed03ad1cf6ee7c872ff99f1888db000fbfd
Vane871 <vane029102@gmail.com>
2020-09-02 20:40:33 -0300

    Entrega 3, cambio2

--- js/product-info.js
+++ js/product-info.js
@@ -7,17 +7,14 @@ function showComentsList(array){
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
-                <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ product.name +`</h4>
-                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        <h4 class="mb-1">`+ coments.user +`</h4>
+                        <small class="text-muted">` + coments.dateTime + ` artículos</small>
                     </div>
                     <div>
-                    <p> ` + product.description + ` </p>
-                    <p> ` + product.currency + " " + product.cost + ` </p>
+                    <p> ` + coments.description + ` </p>
+                    <p> ` + coments.score` </p>
                 </div>
             </div>
         </div>
