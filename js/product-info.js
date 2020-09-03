4be9c23ddd1516d4b9a27e775265d74e320cc0f0
Vane871 <vane029102@gmail.com>
2020-09-02 22:43:30 -0300

    Intento 3, entrega3

--- js/product-info.js
+++ js/product-info.js
@@ -7,23 +7,27 @@ function showComentsList(array){
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
+                <div class="col-3">
+                    <img src="` + productInfo.imgSrc + `" alt="` + productInfo.description + `" class="img-thumbnail">
+                </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ coments.user +`</h4>
-                        <small class="text-muted">` + coments.dateTime + ` artículos</small>
+                        <h4 class="mb-1">`+ productInfo.name +`</h4>
+                        <small class="text-muted">` + productInfo.soldCount + ` artículos</small>
                     </div>
                     <div>
-                    <p> ` + coments.description + ` </p>
-                    <p> ` + coments.score` </p>
+                    <p> ` + productInfo.category + ` </p>
+                    <p> ` + productInfo.currency + " " + productInfo.cost + ` </p>
                 </div>
             </div>
         </div>
     </div>
-        `//Función que se ejecuta una vez que se haya lanzado el evento de
+        `
+//Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
            let productsArray = resultObj.data;
